4661247af8bfe609afd7ab76a03dd26a7cfb5144
Rifat Jahan <mdshantorjbd@gmail.com>
2023-03-22 19:25:03 +0600

    initial commit

---
+++ 1s_assignment.js
@@ -0,0 +1,14 @@
+
+
+// 1st Assignment module 1 ...
+
+function multiplecationTable(number){
+   
+    for(var i=1; i<=10; i++){
+        var product = number +"x"+ i +"="+ number * i ;
+        console.log(product);
+    }
+
+}
+
+var number = multiplecationTable(3);
